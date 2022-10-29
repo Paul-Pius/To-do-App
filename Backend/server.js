1ecd77841dcc94c2d578a9ac3e00505b1e4321b2
yoppeep <paulpius55@gmail.com>
2022-10-29 16:23:11 +0100

    third commit

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+const express = require ("express");
+const mongoose = require ("mongoose");
+const cors = require("cors")
+
+const routes = require("./routes/ToDoRoutes");
+
+require ("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.port || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(() => {console.log("Mongodb connected successfully...");})
+.catch((err) => {console.log(err);})
+
+app.use(routes);
+
+
+
+
+
+app.listen(PORT, () => {console.log(`server running on: ${PORT}`);});
