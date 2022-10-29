729b61143510950ea641b9d1038aacc34c464172
yoppeep <paulpius55@gmail.com>
2022-10-29 16:20:20 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Todo App</h1>
+        <div className="top">
+          <input type="text" placeholder="Add ToDos..." />
+
+          <div className="add">Add</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
